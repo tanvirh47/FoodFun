30c292848306514b7b2a35188e681fb8fdd91d7c
tanvirh <tanvirh934@gmail.com>
2024-07-13 02:14:31 +0600

    email special
    added in home spceial dishes,email, in contact

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -34,6 +38,37 @@ namespace WebApplication1.Controllers
 
             return View();
         }
+        [HttpPost]
+        public ActionResult Contact(string Name, string Email, string Subject, string Message)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress("tanvirh934@gmail.com");
+                mail.To.Add("tanvirh934@gmail.com");
+                mail.Subject = Subject;
+                mail.Body = $"Name: {Name}\nEmail: {Email}\nMessage: {Message}";
+                mail.IsBodyHtml = false;
+
+                SmtpClient smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    Credentials = new NetworkCredential("tanvirh934@gmail.com", "jmlwvsvazwropqjx"),
+                    EnableSsl = true
+                };
+
+                smtp.Send(mail);
+                ViewBag.SuccessMessage = "Email sent successfully!";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = "Error sending email: " + ex.Message;
+            }
+
+            return View();
+        }
+
         public ActionResult Menu()
         {
             var foodItems = db.FoodItems.ToList(); 
